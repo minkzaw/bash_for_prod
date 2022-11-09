c104f05940d89b98273004fb66221bb157bb4037
minkzaw <minkhantzaw.personal@gmail.com>
2022-11-09 17:22:12 +0000

    Almost ready to reset for NC(docker)

--- 2fa-recover.sh
+++ 2fa-recover.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo """This script is meant for the recovery process of 2FA in containerized nextcloud application!"""
-read -p "Which user would you like to query? " user_query
+echo -e """This script is meant for the recovery process of 2FA in containerized nextcloud application!\n"""
 read -n 1 -p "Would you like to continue the script? (y/N) " result
-echo -e "\n"
+echo ""
+read -p "Which user would you like to query? " user_query
+echo ""
 if [ "${result^^}" == "Y" ] && [ ${#user_query} -gt 2 ]; then
     read -p "Please enter the DB container name specifically: " db_container_name
     query_db=$(sudo docker exec -it $db_container_name /bin/bash -c "mysql -u root -proot nextcloud -e 'select * from oc_users'| grep -i $user_query | awk -F ' ' '{print $1}'")
@@ -11,8 +12,6 @@ if [ "${result^^}" == "Y" ] && [ ${#user_query} -gt 2 ]; then
     list_provider=$(sudo docker exec --user www-data --workdir /var/www/html -it $app_container_name /bin/bash -c "php occ twofactorauth:state $user_id")
     if [ $? == 0 ]; then
 
-        #provider="totp"
-        #show_totp=$(echo $list_provider | grep -q $provider)
         provider="totp"
         status="Enabled"
         filter_totp=$(echo $list_provider | grep -q $status -A 2 | grep -q $provider)
@@ -20,7 +19,6 @@ if [ "${result^^}" == "Y" ] && [ ${#user_query} -gt 2 ]; then
         if [ $? == 0 ]; then
             
             echo -e "\n\n$list_provider"
-            #echo -e "\n\n$show_totp"
             read -n 1 -p "Would you like to reset the $provider for $user_id? (y/N) " reset
             echo ""
 
@@ -34,7 +32,7 @@ if [ "${result^^}" == "Y" ] && [ ${#user_query} -gt 2 ]; then
                 exit 1
 
         fi
-            echo "$provider didn't enable for $user_id"
+            echo -e "\n${provider^^} didn't enable for $user_id"
             exit 1
         
     fi
